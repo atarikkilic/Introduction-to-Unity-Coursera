07b0782740b3baa28be12147300c062ebd36f5cd
Tarik Kilic <tarikkilic07@gmail.com>
2021-12-21 17:06:48 +0300

    Clamp the character
    Clamp a game object's position to keep it inside the screen

--- MouseInput/Assets/scripts/Character.cs
+++ MouseInput/Assets/scripts/Character.cs
@@ -7,10 +7,20 @@ using UnityEngine;
 /// </summary>
 public class Character : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // saved for efficiency
+    float colliderHalfWidth;
+    float colliderHalfHeight;
+
+    /// <summary>
+    ///  Use this for initialization
+    /// </summary>
+    
     void Start()
     {
-        
+        //save for efficiency
+        BoxCollider2D collider = GetComponent<BoxCollider2D>();
+        colliderHalfWidth = collider.size.x / 2;
+        colliderHalfHeight = collider.size.y / 2;
     }
 
     // Update is called once per frame
@@ -21,6 +31,35 @@ public class Character : MonoBehaviour
         position.z = -Camera.main.transform.position.z;
         position = Camera.main.ScreenToWorldPoint(position);
 
-        gameObject.transform.position = position;
+        // move character to mouse position and clamp in screen
+        transform.position = position;
+        ClampInScreen();
+    }
+
+    /// <summary>
+    ///  Clamps the character in the screen
+    /// </summary>
+    void ClampInScreen()
+    {
+        // clamp position as necessary
+        Vector3 position = transform.position;
+        if (position.x - colliderHalfWidth < ScreenUtils.ScreenLeft)
+        {
+            position.x = ScreenUtils.ScreenLeft + colliderHalfWidth;
+        }
+        else if (position.x + colliderHalfWidth > ScreenUtils.ScreenRight)
+        {
+            position.x = ScreenUtils.ScreenRight - colliderHalfWidth;
+        }
+        else if (position.y + colliderHalfHeight > ScreenUtils.ScreenTop)
+        {
+            position.y = ScreenUtils.ScreenTop - colliderHalfWidth;
+        }
+        else if (position.y + colliderHalfHeight < ScreenUtils.ScreenBottom)
+        {
+            position.y = ScreenUtils.ScreenBottom + colliderHalfWidth;
+        }
+
+        transform.position = position;
     }
 }
