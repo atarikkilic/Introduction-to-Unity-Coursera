0de8540ffa6e7260bde60b43828bf26ff1fcf8d3
Tarik Kilic <tarikkilic07@gmail.com>
2021-12-22 22:53:32 +0300

    Spawn n Explode
    left mouse click to spawn and right mouse click to explode

--- Spawn and Explode/Assets/scripts/MouseButtonProcessor.cs
+++ Spawn and Explode/Assets/scripts/MouseButtonProcessor.cs
@@ -22,8 +22,50 @@ public class MouseButtonProcessor : MonoBehaviour
 	void Update()
 	{
         // spawn teddy bear as appropriate
+        if (Input.GetAxis("SpawnTeddyBear") > 0)
+        {
+            // only spawn teddy bear on first frame of input
+            if (!spawnInputOnPreviousFrame)
+            {
+                // set input flag
+                spawnInputOnPreviousFrame = true;
 
+                // set instantiate position
+                Vector3 mousePosition = Input.mousePosition;
+                mousePosition.z = -Camera.main.transform.position.z;
+                mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+                // instantiate teddy bear prefab
+                GameObject.Instantiate(prefabTeddyBear, mousePosition, Quaternion.identity);
+            }
+        }
+        else
+        {
+            // no spawn teddy bear input
+            spawnInputOnPreviousFrame = false;
+        }
         // explode teddy bear as appropriate
-		
-	}
+        if (Input.GetAxis("ExplodeTeddyBear") > 0)
+        {
+            // only explode teddy bear on first frame of input
+            if (!explodeInputOnPreviousFrame)
+            {
+                // set input flag
+                explodeInputOnPreviousFrame = true;
+
+                // try to find a teddy bear
+                GameObject teddyBear = GameObject.FindWithTag("TeddyBear");
+                if (teddyBear != null)
+                {
+                    Instantiate(prefabExplosion, teddyBear.transform.position, Quaternion.identity);
+                    Destroy(teddyBear);
+                }
+            }
+        }
+        else
+        {
+            // no explode teddy bear input
+            explodeInputOnPreviousFrame = false;
+        }
+    }
 }
