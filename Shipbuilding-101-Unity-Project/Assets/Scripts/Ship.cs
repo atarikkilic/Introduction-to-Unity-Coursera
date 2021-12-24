5b3215a1c9c9aba81bdeee2fead1de8f967556e9
Tarik Kilic <tarikkilic07@gmail.com>
2021-12-24 22:10:23 +0300

    Step 4: Make the ship wrap
    When the ship leaves the screen on the right it should wrap back into the screen from the left.

--- Shipbuilding-101-Unity-Project/Assets/Scripts/Ship.cs
+++ Shipbuilding-101-Unity-Project/Assets/Scripts/Ship.cs
@@ -8,11 +8,15 @@ public class Ship : MonoBehaviour
     Rigidbody2D shipRigidbody;
     Vector2 thrustDirection = new Vector2 (1,0);
     const float ThrustForce = 3;
+    CircleCollider2D shipCollider;
+    float shipColliderRadius;
 
     // Start is called before the first frame update
     void Start()
     {
         shipRigidbody = GetComponent<Rigidbody2D>();
+        shipCollider = GetComponent<CircleCollider2D>();
+        shipColliderRadius = shipCollider.radius;
     }
 
     // Update is called once per frame
@@ -30,5 +34,23 @@ public class Ship : MonoBehaviour
         {
             shipRigidbody.AddForce(position + ThrustForce * thrustDirection * Time.deltaTime);
         }
+    }
+
+    /// <summary>
+    /// Called when the ship becomes invisible
+    /// </summary>
+    void OnBecameInvisible()
+    {
+        if (position.x - shipColliderRadius <= ScreenUtils.ScreenLeft)
+		{
+			position.x = ScreenUtils.ScreenRight + shipColliderRadius;
+		}
+
+		else if (position.x + shipColliderRadius >= ScreenUtils.ScreenRight)
+		{
+			position.x = ScreenUtils.ScreenLeft - shipColliderRadius;
+		}
+
+        transform.position = position;
     }
 }
